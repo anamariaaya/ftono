ae3c4f93ba58494fb7314c6df87f9eed56fa9c88
anamariaaya <39868590+anamariaaya@users.noreply.github.com>
2024-03-05 20:12:39 +0100

    Add contracts info to each card on admin dashboard

--- controllers/Filmtono/ContractsController.php
+++ controllers/Filmtono/ContractsController.php
@@ -4,26 +4,30 @@ namespace Controllers\Filmtono;
 
 use MVC\Router;
 use Model\Empresa;
+use Model\Usuario;
 use Model\CTRMusical;
 use Model\CTRArtistico;
 use Model\PerfilUsuario;
+use Model\ContratosUsuario;
 
 class ContractsController{
     public static function index(Router $router){
         isAdmin();
         $titulo = 'contracts_main-title';
-        $contratosMusical = CTRMusical::all();
-        $contratosArtistico = CTRArtistico::all();
-        $contratos = CTRMusical::unionTables('ctr_musical', 'ctr_artistico');
-        $empresas = Empresa::all();
-        $perfilUsuario = PerfilUsuario::all();
+        $contratosMusical = CTRMusical::All();
+        $contratosArtistico = CTRArtistico::All();
+        $consulta = 'SELECT ctr.id, ctr.fecha, ctr.nombre_doc, u.nombre AS nombre, u.apellido AS apellido, ';
+        $consulta .= 'e.empresa AS empresa ';
+        $consulta .= 'FROM (SELECT * FROM ctr_musical UNION ALL SELECT * FROM ctr_artistico) AS ctr ';
+        $consulta .= 'JOIN perfil_usuario pu ON pu.id_usuario = ctr.id_usuario ';
+        $consulta .= 'JOIN usuarios u ON u.id = pu.id_usuario ';
+        $consulta .= 'JOIN empresa e ON e.id = pu.id_empresa;';
+        $contratos = ContratosUsuario::consultarSQL($consulta);
 
         $router->render('/admin/contracts/index',[
             'titulo' => $titulo,
             'contratosMusical' => $contratosMusical,
             'contratosArtistico' => $contratosArtistico,
-            'perfilUsuario' => $perfilUsuario,
-            'empresas' => $empresas,
             'contratos' => $contratos
         ]);
     }
--- views/admin/contracts/index.php
+++ views/admin/contracts/index.php
@@ -13,25 +13,36 @@
 
 <div class="cards">
     <div class="cards__container">
-            <?php foreach( $contratos as $contrato): ?>       
-                <a href="/music/albums/current?id=<?php echo $contrato->id;?>">
+            <?php
+             foreach( $contratos as $contrato): ?>       
+                <a href="/filmtono/contracts/current?id=<?php echo $contrato->id;?>">
                     <div class="cards__card">
                         <div class="cards__info">
-                            <p class="cards__text"> <?php echo $contrato->nombre_doc;?></p>
-                            <p class="cards__text"> <?php echo $contrato->fecha;?></p>
-                            <p class="cards__text"> <?php 
-                                foreach($perfilUsuario as $perfilUsuario){
-                                    if($perfilUsuario->id_empresa == $contrato->id_empresa){
-                                        echo $perfilUsuario->id_empresa;
-                                    }
-                                                              
-                            }?></p>
+                            <p class="cards__text">
+                                <span>{%contracts_user-name%}: </span>
+                                <?php echo $contrato->nombre.' '.$contrato->apellido;?></p>
+                            <p class="cards__text">
+                                <span>{%contracts_empresa%}: </span>
+                                <?php echo $contrato->empresa;?></p>
+                            <p class="cards__text">
+                                <span>{%contracts_type%}: 
+                                </span>
+                                <?php
+                                $filename = $contrato->nombre_doc;
+                                $parts = explode('-', $filename);
+                                $type = $parts[2];
+                                if($type == 'music'):?>
+                                    {%contracts_type-music%}
+                                <?php else: ?>
+                                    {%contracts_type-artistic%}
+                                <?php endif; ?>
+                            </p>
+                            <p class="cards__text">
+                                <span>{%contracts_fecha%}: </span>
+                                <?php echo $contrato->fecha;?></p>
                         </div>
                         <div class="cards__actions">
-                            <!-- <a href="/music/albums/update?id=<?php echo $album->id;?>" class="btn-update">
-                                <i class="fa-solid fa-pencil"></i>
-                            </a> -->
-                            <button id="eliminar-usuario" class="btn-delete" value="<?php echo $contrato->id;?>">
+                            <button id="eliminar-contrato" class="btn-delete" value="<?php echo $contrato->id;?>">
                                 <i class="fa-solid fa-trash-can no-click"></i>
                             </button>
                         </div>
