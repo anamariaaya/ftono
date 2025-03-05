21158002f9a59ada031f67ba6347a8738dad37c2
anamariaaya <39868590+anamariaaya@users.noreply.github.com>
2025-03-05 18:27:37 +0100

    change function to display menu language and move logo to the top on mobile

--- public/build/js/UI/language.js
+++ public/build/js/UI/language.js
@@ -1,13 +1,9 @@
 import { selectLang, divLang, btnLang } from './selectores.js';
 
 export function chooseLang(){    
-    //Mostrar el select de idioma
-    divLang.onmouseover = function(){
-        selectLang.classList.remove('no-display');
-    };
-    selectLang.onmouseout = function(){
-        selectLang.classList.add('no-display');
-    };
+    divLang.onclick = function(){
+        selectLang.classList.toggle('no-display');
+    }
 
     btnLang.forEach((btn) => {
         btn.addEventListener('click', (e) => {
